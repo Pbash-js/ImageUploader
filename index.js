8faaef78529d8d1b237aa58ffe12d54a8add545f
Purnanshu Borkar <purnanshuborkarofficial@gmail.com>
2021-03-24 12:06:54 +0530

    make it better

--- index.js
+++ index.js
@@ -61,6 +61,22 @@ if (process.env.NODE_ENV === "production") {
   //serve the static file
 
   app.get("*", (req, res) => {
+    fs.readdir(
+      path.resolve(__dirname, "fileuploader", "build", "uploads"),
+      (err, files) => {
+        if (err) throw err;
+        if (files) {
+          files.forEach((file) => {
+            fs.unlink(
+              path.resolve(__dirname, "fileuploader", "build", "uploads", file),
+              (err) => {
+                if (err) throw err;
+              }
+            );
+          });
+        }
+      }
+    );
     res.sendFile(
       path.resolve(__dirname, "fileuploader", "build", "index.html")
     );
@@ -68,21 +84,5 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.listen(PORT, () => {
-  fs.readdir(
-    path.resolve(__dirname, "fileuploader", "src", "uploads"),
-    (err, files) => {
-      if (err) throw err;
-      if (files) {
-        files.forEach((file) => {
-          fs.unlink(
-            path.resolve(__dirname, "fileuploader", "src", "uploads", file),
-            (err) => {
-              if (err) throw err;
-            }
-          );
-        });
-      }
-    }
-  );
   console.log("Server has started!");
 });
