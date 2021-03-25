8448440098a4d3a410f7e3992ffb845ebd3f2fc8
Purnanshu Borkar <purnanshuborkarofficial@gmail.com>
2021-03-25 14:16:17 +0530

    make it better

--- index.js
+++ index.js
@@ -18,7 +18,27 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.post("/upload", (req, res) => {
+app.post("/upload", async (req, res) => {
+  try {
+    fs.readdir(
+      path.resolve(__dirname, "fileuploader", "build", "uploads"),
+      (err, files) => {
+        if (err) console.log(err);
+        if (files) {
+          files.forEach((file) => {
+            fs.unlink(
+              path.resolve(__dirname, "fileuploader", "build", "uploads", file),
+              (err) => {
+                if (err) console.log(err);
+              }
+            );
+          });
+        }
+      }
+    );
+  } catch (error) {
+    res.send("server error");
+  }
   try {
     const form = new formidable.IncomingForm();
     form.parse(req);
@@ -40,7 +60,7 @@ app.post("/upload", (req, res) => {
       file.path = filepath;
     });
 
-    form.on("file", (name, file) => {
+    form.on("end", (name, file) => {
       console.log("Uploaded file" + file.name);
       res.send(filepath);
     });
@@ -71,22 +91,6 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("fileuploader/build"));
 
   app.get("*", (req, res) => {
-    fs.readdir(
-      path.resolve(__dirname, "fileuploader", "build", "uploads"),
-      (err, files) => {
-        if (err) throw err;
-        if (files) {
-          files.forEach((file) => {
-            fs.unlink(
-              path.resolve(__dirname, "fileuploader", "build", "uploads", file),
-              (err) => {
-                if (err) throw err;
-              }
-            );
-          });
-        }
-      }
-    );
     res.sendFile(
       path.resolve(__dirname, "fileuploader", "build", "index.html")
     );
